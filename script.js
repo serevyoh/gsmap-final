defc75b6f602e91ec9dbccf249f0ceceb745a23c
serevyoh <da.serevius@gmail.com>
2026-06-12 19:43:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1117,25 +1117,28 @@ if(imagen){
 }
 // ================= LEYENDA =================
 const leyendaCabecera =
-  document.getElementById(
-    "leyendaCabecera"
-  );
+document.getElementById("leyendaCabecera");
 const leyendaContenido =
-  document.getElementById(
-    "leyendaContenido"
+document.getElementById("leyendaContenido");
+if(
+  leyendaCabecera &&
+  leyendaContenido
+){
+  leyendaCabecera.addEventListener(
+    "click",
+    () => {
+      if(
+        leyendaContenido.style.display
+        === "none"
+      ){
+        leyendaContenido.style.display =
+          "block";
+      }else{
+        leyendaContenido.style.display =
+          "none";
+      }
+    }
   );
-leyendaCabecera.onclick = () => {
-  if(
-    leyendaContenido.style.display
-    === "none"
-  ){
-    leyendaContenido.style.display =
-      "block";
-  }else{
-    leyendaContenido.style.display =
-      "none";
-  }
-};
 
 }
 };
